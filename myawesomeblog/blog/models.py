ee3f7781bebaed4d7c42f46e91ae03fde5a115fa
Andrei <matyshenko-1989@tut.by>
2021-10-20 19:42:18 +0300

    add blog

--- myawesomeblog/blog/models.py
+++ myawesomeblog/blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=150)
+    blog_date = models.DateTimeField(auto_now = False , auto_now_add = False)
+    blog_text = models.TextField(blank=True)
+    blog_image = models.ImageField(upload_to='event_image/')
